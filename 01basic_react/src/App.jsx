5cc1d6c548780902431d3aa5a5b0ef9c63f2f296
Avni15Sharma <avnishrm15@gmail.com>
2024-12-29 00:45:42 +0530

    adding 01basic_react

---
+++ 01basic_react/src/App.jsx
@@ -0,0 +1,22 @@
+    //prefer documentation to get started
+    // npx use nahi kr rahe hain 
+    // npx create react app krke nahi kr rahe hain
+    // hum use kr rahe hain vite jo ki ek bundler hain npm create vite@latest 
+    // sbse pehle read krte hai package.json 
+    // usme scripts kese run krni hai pta chlta hai 
+    // jese ki npm run install to install node modules 
+    // npm run dev
+
+import Hello from "./hello"
+function App() {
+   
+  return (
+    <>
+    <h1>React| Avni </h1>
+    <Hello/>
+    </>
+    
+  )
+}
+
+export default App
---
+++ 01basic_react/src/Hello.jsx
@@ -0,0 +1,11 @@
+// new component -> name .jsx aur function capital letter se start krenge
+// best practice -> file name bhi capital se start ho 
+// App.jsx ek hi element return krta hai toh isliye
+// empty <> se surround kr dete h 
+// internally multiple elements ho skte hain
+function Hello(){
+    return(
+        <h2>hello</h2>
+    )
+}
+export default Hello
